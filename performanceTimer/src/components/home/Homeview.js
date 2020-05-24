69cf75907d62aabb7f541ec1594f3516b234221d
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2020-05-24 21:06:56 +0100

    fcomp to classcomp

--- performanceTimer/src/components/home/Homeview.js
+++ performanceTimer/src/components/home/Homeview.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { Component } from 'react'
 import { 
   View , 
   Text , 
@@ -11,34 +12,37 @@ import { homeStyles } from './homeViewStyles'
 import Greetings from '../utils/Greetings'
 import GetFecha from '../utils/date'
 
-const Homeview = () => {  
-  return (
-    <View style={homeStyles.container}>
-      <ImageBackground
-        source={require('./lion.webp')}
-        resizeMode='cover'
-        style={ homeStyles.imgBackg }
-      >
-        <View style={{ flex: 0.90 }}>
-          <Text style={homeStyles.h1}><Greetings /></Text>
-        </View>
-        <View style={{ flex: 1}}>
-          <TouchableOpacity 
-            onPress={() => console.log('Pressed')}
-            style={homeStyles.touch}
-          >
-            <Image 
-                source={require('./test.webp')}
-                style={ homeStyles.icon } 
-            />
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 1 }}>
-          <GetFecha />
-        </View>
-      </ImageBackground>
-    </View>
-  )
+
+class Homeview extends Component {
+  render() {
+    return (
+      <View style={homeStyles.container}>
+        <ImageBackground
+          source={require('./lion.webp')}
+          resizeMode='cover'
+          style={ homeStyles.imgBackg }
+        >
+          <View style={{ flex: 0.90 }}>
+            <Text style={homeStyles.h1}><Greetings /></Text>
+          </View>
+          <View style={{ flex: 1}}>
+            <TouchableOpacity 
+              onPress={() => console.log('Pressed')}
+              style={homeStyles.touch}
+            >
+              <Image 
+                  source={require('./test.webp')}
+                  style={ homeStyles.icon } 
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1 }}>
+            <GetFecha />
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
 }
 
 export default Homeview;
